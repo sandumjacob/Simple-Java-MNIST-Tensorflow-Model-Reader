a810a5d3c2b65f59d287afa8b96f36e3488c6fca
sandumjacob <sandumjacobpersonal@gmail.com>
2018-06-02 20:32:04 -0500

    Prints Number

--- src/com.jacobsandum/java/Test.java
+++ src/com.jacobsandum/java/Test.java
@@ -67,6 +67,18 @@ public class Test {
         result.copyTo(resultFloat);
         System.out.println("RESULT FLOAT");
         System.out.println(Arrays.deepToString(resultFloat));
+
+        int[] format = new int[]{0,1,2,3,4,5,6,7,8,9};
+        int resultAsInt = 0;
+        float greatest = resultFloat[0][0];
+        for (int i = 0; i < resultFloat[0].length; i++) {
+            if (resultFloat[0][i] > greatest) {
+                greatest = resultFloat[0][i];
+                resultAsInt = format[i];
+            }
+        }
+        System.out.println("FINAL RESULT");
+        System.out.println("Digit= " + resultAsInt);
     }
 
     public static INDArray getNormalizedImage(String path) throws IOException {
